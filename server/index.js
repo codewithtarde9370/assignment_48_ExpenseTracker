cf14404af5329be5e0a93196746391265e4e762d
codewithtarde9370 <teja36491@gmail.com>
2024-07-28 09:55:48 +0530

    added models

--- server/index.js
+++ server/index.js
@@ -1,33 +1,44 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import mongoose from 'mongoose'
-import cors from 'cors'
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
-app.use(express.json);
+app.use(express.json());
 app.use(cors());
 
-const connectDB = async () => {
-    const conn = await mongoose.connect(process.env.MONGODB_URL)
+import { postSignup, postLogin } from './controllers/user.js';
 
-    if(conn){
-        console.log("MongoDB connected successfully!✅");
-    }
-    else{
-        console.log("Failure in the DataBase Connection!❌");
-    }
-}
+import { postTransaction, getTransactions, deleteTransaction } from "./controllers/transaction.js";
+
+
+// Database connection
+const connectDB = async () =>{
+  const conn = await mongoose.connect(process.env.MONGODB_URL)
+
+  if (conn) {
+    console.log(`MongoDB connected successfully ✅`);
+  }
+};
 connectDB();
 
-app.get("/", (req, res) => {
-    res.json({
-        message:"Welcome To The EXPENSE TRACKER APP!!"
-    })
+
+app.get('/', (req, res) => {
+  res.json({
+    message: `Welcome to my EXPENSE TRACKER API`
+  })
 })
+
+app.post("/signup", postSignup)
+app.post("/login", postLogin)
+
+app.post("/transaction", postTransaction)
+app.get("/transactions", getTransactions)
+app.delete("/transaction/:id", deleteTransaction)
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`server is running on the port ${PORT}`)
+  console.log(`Server is running on port ${PORT}`);
 })
--- server/models/users.js
+++ server/models/users.js
@@ -1,5 +1,8 @@
 import { Schema, model } from "mongoose";
 
+// credit means adding money in the acc
+// debit means subtracting money from the acc
+
 const userSchema = new Schema({
   fullName: {
     type: String,
